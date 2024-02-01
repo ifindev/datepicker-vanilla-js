8482e37396ef972bbe565950d9c8bdb4ad7df97f
Muhammad Arifin <muhammad.arifin@nri.co.id>
2024-02-02 01:36:17 +0700

    feat: add render dates & date selection handler

---
+++ requirement.md
@@ -0,0 +1,10 @@
+## UIs
+
+![Datepicker](datepicker.png)
+
+## Requirements
+
+- Can show current month & year for the calendar
+- Can navigate back one month or navigate back one year
+- Can navigate up one month or navigate up one year
+- Can render calendar
--- script.js
+++ script.js
@@ -18,6 +18,8 @@ const MONTHS = {
   11: 'December',
 };
 
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 // ========================================= util
 function getDates(month, year) {
   const date = new Date(year, month, 1);
@@ -52,6 +54,11 @@ function nextYear() {
   render();
 }
 
+function handleSelect(date) {
+  selected = date;
+  render();
+}
+
 // ========================================= render
 
 function renderCurrent() {
@@ -59,8 +66,103 @@ function renderCurrent() {
   document.getElementById('current').innerHTML = currentDate;
 }
 
+function renderCalendar() {
+  const currentMonth = current.getMonth();
+  const currentYear = current.getFullYear();
+
+  const calendar = document.getElementById('calendar');
+  calendar.innerHTML = '';
+  calendar.className = 'grid grid-cols-7 gap-2 mt-5';
+
+  // generate days
+  for (i = 0; i < DAYS.length; i++) {
+    const day = document.createElement('span');
+    day.className = 'text-center py-2';
+    day.append(DAYS[i]);
+    calendar.append(day);
+  }
+
+  // generate dates
+  const dates = getDates(currentMonth, currentYear);
+
+  const totalDays = 7;
+  const firstDayOfTheMonth = dates[0].getDay();
+  const lastDayOfTheMonth = dates[dates.length - 1].getDay() + 1;
+
+  const padStart = firstDayOfTheMonth;
+  const padEnd = totalDays - lastDayOfTheMonth;
+
+  // Add whitespaces in the beginning week of the month
+  for (i = 0; i < padStart; i++) {
+    dates.splice(0, 0, '');
+  }
+
+  // Add whitespaces in the last week of the month
+  for (i = 0; i <= padEnd; i++) {
+    dates.splice(dates.length, 0, '');
+  }
+
+  for (i = 0; i < dates.length - 1; i++) {
+    if (dates[i] === '') {
+      const whitespace = document.createElement('span');
+      whitespace.innerHTML = '';
+      whitespace.className = 'text-center';
+      calendar.append(whitespace);
+      continue;
+    }
+
+    // date is button element so that we can select it later
+    const date = document.createElement('button');
+    date.className = 'text-center py-2 rounded';
+    date.append(dates[i].getDate());
+
+    const baseClass = 'text-center py-2 ';
+    date.className = baseClass;
+
+    // Add style for today
+    if (
+      dates[i].getDate() === current.getDate() &&
+      dates[i].getMonth() === currentMonth &&
+      dates[i].getFullYear() === currentYear &&
+      selected === null
+    ) {
+      date.className = baseClass + 'rounded border bg-blue-600 text-white';
+    }
+
+    if (
+      dates[i].getDate() === current.getDate() &&
+      dates[i].getMonth() === currentMonth &&
+      dates[i].getFullYear() === currentYear &&
+      selected !== null
+    ) {
+      date.className = baseClass + 'rounded border border-gray-600';
+    }
+
+    if (
+      selected &&
+      dates[i].getDate() === selected.getDate() &&
+      dates[i].getMonth() === selected.getMonth() &&
+      dates[i].getFullYear() === selected.getFullYear()
+    ) {
+      date.className = baseClass + 'rounded bg-blue-600 text-white';
+    }
+
+    // use IIFE to get index i to solve the problem with closure
+    date.addEventListener(
+      'click',
+      (function (index) {
+        return function (evt) {
+          handleSelect(dates[index]);
+        };
+      })(i)
+    );
+    calendar.append(date);
+  }
+}
+
 function render() {
   renderCurrent();
+  renderCalendar();
 }
 
 render();
